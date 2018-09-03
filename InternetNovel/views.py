dcea8b026dcb8dd46eec72ed1c3b2d4ba22c7d9c
fanlf <277314127@qq.com>
2018-09-03 22:59:33 +0800

    hard to start

--- InternetNovel/views.py
+++ InternetNovel/views.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponse
+
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'InternetNovel/index.html'
+    context_object_name = 'hot_novel_list'
+
+    def get_queryset(self):
+        return 'Hello World !'
+
+
+class Login:
+    loginTitle = '会员登陆'
